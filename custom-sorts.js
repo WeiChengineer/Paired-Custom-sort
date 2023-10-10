bb4b50ec0b853df621debe1636aec7b2423a576c
Wei Cheng Chen <DrChenWeiCheng@gmail.com>
2023-10-10 13:46:25 -0700

    finished

--- custom-sorts.js
+++ custom-sorts.js
@@ -13,11 +13,25 @@ function validAnagrams(s, t) {
 }
 
 function reverseBaseSort(arr) {
-
+  return arr.sort((a, b) => {
+    const baseA = Math.floor(Math.log10(a) || 0);
+    const baseB = Math.floor(Math.log10(b) || 0);
+    if (baseA === baseB) return a - b;
+    return baseB - baseA;
+  });
 }
 
-function frequencySort(arr) {
 
+function frequencySort(arr) {
+  const freqMap = arr.reduce((acc, num) => {
+    acc[num] = (acc[num] || 0) + 1;
+    return acc;
+  }, {});
+
+  return arr.sort((a, b) => {
+    if (freqMap[a] === freqMap[b]) return b - a;
+    return freqMap[a] - freqMap[b];
+  });
 }
 
 module.exports = [
